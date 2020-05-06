4dbdfca83019caaf95375b34c18a445129678404
Anton Boutkam <anton@nui-boutkam.nl>
2020-05-06 12:53:25 +0200

    [+] Datamodel updates are mostly for generating UI. [+] Moved basic authentication to core part of system. [+] Bugfixes in task manager [+] Added api description to GenericFields (bugfix) [+] Small datamodel changes + new XSD with UI components for cockpit project. [+] Added https://api.nuicart.nl to the build script. [+] Some work on UI builder project. [+] Added .env files for api.nuicart.nl project. [+] Made api.nuicart.nl project more up to date with current situation. [+] Had phpstorm revove a lot of unused imports

--- DesignController.php
+++ DesignController.php
@@ -1,17 +1,33 @@
 <?php
 namespace AdminModules\Apps\App\Ui;
 
+use Core\Layout;
+
 class DesignController extends \Core\MainController
 {
+    function __construct($aGet, $aPost)
+    {
+        Layout::setNavTopVisibility(true);
+        Layout::setSidebarLeftVisibility(true);
+
+
+        parent::__construct($aGet, $aPost);
+    }
+
     function run()
     {
-        $this->addJsFile();
+        $this->addJsFile('/vendor/grapesjs/dist/grapes.min.js');
+        $this->addCssFile('/vendor/grapesjs/dist/css/grapes.min.css');
 
         $aDesignerTopNav = [];
         $aDesigner = [];
 
         return [
             'title' => '',
+            'layout' => [
+                'id' => 'flex',
+                'class' => 'flex'
+            ],
             'top_nav' => $this->parse('Apps/App/Ui/designer_top_nav.twig', $aDesignerTopNav),
             'content' => $this->parse('Apps/App/Ui/designer.twig', $aDesigner),
         ];
